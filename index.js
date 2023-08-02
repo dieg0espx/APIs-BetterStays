270ba31a1d9ace4efa18cf8df8d33b561b7f5815
diegoTrueZenit <diego@truezenithcreative.com>
2023-08-02 14:07:30 -0600

    .

--- index.js
+++ index.js
@@ -239,6 +239,23 @@ app.get("/api/multipleCalendar", async (req,res)=>{
       authorization: 'Bearer ' + token
     }
   };
+  let propertiesID = [
+    {name: "The Pool House",     id:"638a965985cf74003f7b34e6"},
+    {name: "Mariogold",          id:"627c1ef89d63b2003223d578"},
+    {name: "Golfers Retreat",    id:"63670920fe8aa4007565c9d2"},
+    {name: "The Lake House",     id:"63f03e1ce78efd003ec4d7de"},
+    {name: "Sentinel",           id:"62a297f98d4ca500310bf165"},
+    {name: "Oak Park Game Room", id:"643596e34613d0003620b962"},
+    {name: "Landstrom",          id:"627c1f19b8ff0000368578ce"},
+    {name: "Tuneberg",           id:"627c1f09c01e3a00346b803b"},
+    {name: "Mario's Pad",        id:"6435af6b4613d0003623178c"},
+  ]
+  let allIDs = '';
+  for (let i = 0; i < propertiesID.length; i++) {
+    i > 0 ? allIDs += "," + propertiesID[i].id : allIDs = propertiesID[i].id
+  }
+
+
   const { start, end } = req.query;
   fetch('https://open-api.guesty.com/v1/availability-pricing/api/calendar/listings?listingIds=' + allIDs + '&startDate=' + start + '&endDate=' + end, options)
   .then(response => response.json())
